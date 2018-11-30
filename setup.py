82f7aed1e488f111e03f04c9936a8bfc119d12ff
Jakob Jul Elben <vzx151@ku.dk>
2018-11-30 14:49:52 +0100

    add first take on travis

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
-install_requires_list = []
-setup_requires_list = ["pytest-runner"]
-test_requires_list = ["pytest"]
-
 setup(
     name="NumEcon",
     version="0.1.0",
     author="Jakob Jul Elben, Jeppe Druedahl",
     packages=find_packages(),
-    install_requires=install_requires_list,
-    setup_requires=setup_requires_list,
-    test_requires=test_requires_list,
 )
