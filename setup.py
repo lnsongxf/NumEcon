1ae592ffeb2611503567ade2c41f1c630081fd4c
Jakob Jul Elben <elbenjakobjul@gmail.com>
2018-12-02 12:50:46 +0100

    Add black and docs command

--- setup.py
+++ setup.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python
-from setuptools import find_packages, setup
+import distutils
+import os
+import setuptools
+import shlex
+import shutil
+import subprocess
 
-setup(
+
+class BlackCommand(distutils.cmd.Command):
+    """A custom command to format python code"""
+
+    description = "run Black on Python source files"
+    user_options = []
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        """Run command."""
+        command = "python -mblack NumEcon tests"
+
+        self.announce(f"Running command: {command}", level=distutils.log.INFO)
+        subprocess.check_call(shlex.split(command))
+
+
+class DocsCommand(distutils.cmd.Command):
+    """A custom command to format python code"""
+
+    description = "generate documentation"
+    user_options = []
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        """Run command."""
+        command = "make html"
+
+        if os.path.exists("docs/_build") and os.path.isdir("docs/_build"):
+            self.announce(f"Removing build directory", level=distutils.log.INFO)
+            shutil.rmtree("docs/_build")
+
+        self.announce(f"Running command: {command}", level=distutils.log.INFO)
+        subprocess.check_call(shlex.split(command), cwd="docs")
+
+
+setuptools.setup(
     name="NumEcon",
     version="0.1.0",
     author="Jakob Jul Elben, Jeppe Druedahl",
-    packages=find_packages(),
-    package_data={
-        'NumEcon': ['Notebooks/*']
-    },
-    entry_points={
-        'console_scripts': [
-            'NumEcon=NumEcon.__main__:cli'
-        ]
-    }
+    packages=setuptools.find_packages(),
+    package_data={"NumEcon": ["Notebooks/*"]},
+    entry_points={"console_scripts": ["NumEcon=NumEcon.__main__:cli"]},
+    cmdclass={"black": BlackCommand, "docs": DocsCommand},
 )
