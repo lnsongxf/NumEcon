39d600041557781595253d48f7faf7760083ca86
Jakob Jul Elben <elbenjakobjul@gmail.com>
2018-12-02 13:49:17 +0100

    Adds some custom commands to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ import setuptools
 import shlex
 import shutil
 import subprocess
+import threading
+import time
+
+from watchdog import observers
+from watchdog import events
 
 
 class BlackCommand(distutils.cmd.Command):
@@ -54,6 +59,55 @@ class DocsCommand(distutils.cmd.Command):
         self.announce(f"Running command: {command}", level=distutils.log.INFO)
         subprocess.check_call(shlex.split(command), cwd="docs")
 
+class PipWatch(events.PatternMatchingEventHandler):
+    def on_any_event(self, event):
+        subprocess.check_call(shlex.split('pip install .'))
+
+class WatchCommand(distutils.cmd.Command):
+    """A custom command to format python code"""
+
+    description = "watch install"
+    user_options = []
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        event_handler = PipWatch('*.py')
+        observer = observers.Observer()
+        observer.schedule(event_handler, 'NumEcon', recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except KeyboardInterrupt:
+            observer.stop()
+
+class JupyterCommand(distutils.cmd.Command):
+    description = "Jupyter"
+    user_options = []
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        try:
+            threading.Thread(target = lambda: subprocess.run(shlex.split('jupyter notebook NumEcon/Notebooks'))).start()
+            threading.Thread(target = lambda: subprocess.run(shlex.split('python setup.py watch'))).start()
+        except KeyboardInterrupt:
+            pass
+
+
 
 setuptools.setup(
     name="NumEcon",
@@ -62,5 +116,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_data={"NumEcon": ["Notebooks/*"]},
     entry_points={"console_scripts": ["NumEcon=NumEcon.__main__:cli"]},
-    cmdclass={"black": BlackCommand, "docs": DocsCommand},
+    cmdclass={"black": BlackCommand, "docs": DocsCommand, "jupyter": JupyterCommand, "watch": WatchCommand},
 )
